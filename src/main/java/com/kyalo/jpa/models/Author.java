9b9116ba8f907c19472eb0095584922bf6bb81df
kyalo-vibes <kevin.kioko4@gmail.com>
2024-10-21 11:58:53 +0300

    Created Entity

--- src/main/java/com/kyalo/jpa/models/Author.java
+++ src/main/java/com/kyalo/jpa/models/Author.java
@@ -1,18 +1,35 @@
 package com.kyalo.jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Entity
+@Table(name = "AUTHOR_TBL")
 public class Author {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_generator")
+    @SequenceGenerator(name = "author_generator", sequenceName = "author_sequence", allocationSize = 1)
+    //@TableGenerator(name = "author_generator", table = "author_sequence", pkColumnName = "sequence_name", valueColumnName = "sequence_value", initialValue = 1,allocationSize = 1)
     private Integer id;
+
+    @Column(name="f_name")
     private String firstName;
+
     private String lastName;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     private int age;
+
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
 }
