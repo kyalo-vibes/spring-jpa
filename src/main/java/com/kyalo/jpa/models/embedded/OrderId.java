eb36946a276619d51840e9a5e67d1cf10a55f5e0
kyalo-vibes <kevin.kioko4@gmail.com>
2024-10-24 05:03:57 +0300

    Finished Embeddable

--- src/main/java/com/kyalo/jpa/models/embedded/OrderId.java
+++ src/main/java/com/kyalo/jpa/models/embedded/OrderId.java
@@ -1,4 +1,19 @@
 package com.kyalo.jpa.models.embedded;
 
-public class OrderId {
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable// Makes the class an embedded entity
+public class OrderId implements Serializable {
+    private String username;
+
+    private LocalDateTime orderDate;
 }
